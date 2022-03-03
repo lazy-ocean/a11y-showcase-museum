10d0de459157c362c4add4c0964717ea06823b62
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-03-03 01:11:29 +0100

    build: add react-icons library

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,10 +6,6 @@ const Main = () => {
     <main>
       <Container>
         <Header>go build something awesome</Header>
-        <img
-          src="https://c.tenor.com/bCfpwMjfAi0AAAAC/cat-typing.gif"
-          alt="cat typing"
-        />
       </Container>
     </main>
   );
