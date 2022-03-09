0ede6ecc5fb00b2f7c382b9182e5d6a5b83649cb
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-03-09 21:16:38 +0100

    feat: add breakpoints custom hook

---
+++ src/utils/useBreakpoint.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const BREAKPOINTS = {
+  mobile: 600,
+  tablet: 810,
+  desktop: 1920,
+};
+
+interface checkBreakpointProps {
+  isMobile?: boolean;
+  isTablet?: boolean;
+  isDesktop?: boolean;
+}
+
+const useBreakpoint = (): checkBreakpointProps => {
+  const [width, setWidth] = useState<number>(0);
+
+  const handleWindowSizeChange = (): void =>
+    window ? setWidth(window.innerWidth) : setWidth(0);
+
+  useEffect(() => {
+    handleWindowSizeChange();
+    window.addEventListener("resize", handleWindowSizeChange);
+
+    return (): void => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile: boolean = width <= BREAKPOINTS.mobile;
+  const isTablet: boolean = width <= BREAKPOINTS.tablet;
+  const isDesktop: boolean = width > BREAKPOINTS.tablet;
+
+  return { isMobile, isTablet, isDesktop };
+};
+
+export default useBreakpoint;
