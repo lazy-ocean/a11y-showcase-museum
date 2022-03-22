ad22596360b65bc5327182730a6b1c9794a102c0
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-03-22 17:30:37 +0100

    feat: finish subscription form

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { PrimaryButton } from "../Buttons/Buttons";
+import { TertiaryHeading } from "../Typography";
 
 export const StyledFooter = styled.footer`
   margin-top: ${({ theme }) => theme.spacing.huge};
@@ -21,7 +22,7 @@ export const FooterMenu = styled.nav`
   font-size: 1.8rem;
   display: flex;
   flex-direction: column;
-  gap: 1.6rem;
+  gap: 2rem;
   margin-bottom: ${({ theme }) => theme.spacing.medium};
 `;
 
@@ -41,7 +42,7 @@ export const SubscribeInput = styled.input`
   background-color: transparent;
   border: none;
   border-bottom: 1px solid black;
-  padding: 1rem 0;
+  padding: 0 0 1rem 0;
   margin: ${({ theme }) => theme.spacing.small};
   margin-left: 0;
   font-size: 1.8rem;
@@ -50,15 +51,32 @@ export const SubscribeInput = styled.input`
 
 export const SubscribeBtn = styled(PrimaryButton)`
   padding: 1rem 2rem;
+  align-self: baseline;
 `;
 
-export const Wrapper = styled.div`
+export const ColumnWrapper = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
 `;
 
 export const Checkbox = styled.input`
   width: 24px;
   height: 24px;
   margin-right: ${({ theme }) => theme.spacing.small};
+`;
+
+export const SubscribedText = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing.small};
+
+  svg {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing.small} 0;
 `;
--- src/components/Footer/SubscriptionForm.tsx
+++ src/components/Footer/SubscriptionForm.tsx
@@ -1,34 +1,59 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { LanguageContext } from "../../utils/LanguageContext";
 import {
   SubscribeInput,
   SubscribeBtn,
-  Wrapper,
+  ColumnWrapper,
   Checkbox,
+  SubscribedText,
+  Wrapper,
 } from "./Footer.styled";
 import { TertiaryHeading, RegularText } from "../Typography";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 const SubscriptionForm = () => {
+  const [subscribed, setSubscribed] = useState<boolean>(false);
   const { dictionary } = useContext(LanguageContext);
 
-  return (
-    <form action="">
+  const handleSubscribe = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setSubscribed(true);
+  };
+
+  return !subscribed ? (
+    <form onSubmit={handleSubscribe}>
       <TertiaryHeading>{dictionary.footer.subscribeHeader}</TertiaryHeading>
       <Wrapper>
-        <SubscribeInput
-          type="email"
-          name={dictionary.login.email}
-          placeholder={dictionary.login.email}
-        />
+        <ColumnWrapper>
+          <SubscribeInput
+            type="email"
+            name={dictionary.login.email}
+            placeholder={dictionary.login.email}
+            aria-label={dictionary.login.email}
+          />
+          <Wrapper>
+            <Checkbox
+              type="checkbox"
+              name={dictionary.footer.gdpr}
+              id="gdpr"
+              required
+              aria-required={true}
+              aria-label={dictionary.footer.gdpr}
+              aria-checked="false"
+            />
+            <RegularText as="label" htmlFor="gdpr">
+              {dictionary.footer.gdpr}
+            </RegularText>
+          </Wrapper>
+        </ColumnWrapper>
         <SubscribeBtn type="submit">{dictionary.footer.subscribe}</SubscribeBtn>
       </Wrapper>
-      <Wrapper>
-        <Checkbox type="checkbox" name={dictionary.footer.gdpr} id="gdpr" />
-        <RegularText as="label" htmlFor="gdpr">
-          {dictionary.footer.gdpr}
-        </RegularText>
-      </Wrapper>
     </form>
+  ) : (
+    <SubscribedText aria-live="polite">
+      <AiFillCheckCircle aria-hidden="true" />
+      <TertiaryHeading>Thank you!</TertiaryHeading>
+    </SubscribedText>
   );
 };
 
