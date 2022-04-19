90636648ce8d251b7b9b85a7fdbceb68cc014628
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-04-19 17:35:04 +0200

    feat(utils): add useKeydown custom hook

---
+++ src/utils/useKeydown.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+const useKeydown = (key: string, onEvent: () => void): void => {
+  const handleClick = (event: KeyboardEvent): void => {
+    if (event.code === "ArrowUp" || event.code === "ArrowDown")
+      event.preventDefault();
+    if (event.code === key) onEvent();
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", handleClick);
+    return () => {
+      document.removeEventListener("keydown", handleClick);
+    };
+  });
+};
+
+export default useKeydown;
