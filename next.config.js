28768c267dd568a8c5cf4ef2de0cb09a607b0be7
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-04-25 21:09:48 +0200

    feat: add CSP because why not

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  font-src 'self';
+  style-src 'unsafe-inline';
+  img-src 'self' data: https:;  
+`;
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   i18n: {
     locales: ["en", "ru"],
